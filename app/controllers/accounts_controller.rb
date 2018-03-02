f3425084a708ba410c3dae66041303ece17e3608
Ian Harwell <sirharwell@gmail.com>
2018-03-02 12:10:12 -0700

    users

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,13 +1,42 @@
 class AccountsController < ApplicationController
+
   def index
+    @accounts = current_user.accounts
   end
 
   def show
+    @account = current_user.accounts.find(params[:id])
   end
 
   def new
+    @account = Account.new
+  end
+
+  def create
+    @account = current_user.accounts.new(account_params)
+    if @account.save
+      flash[:success] = 'Account Created!'
+      redirect_to accounts_path
+    else
+      flash[:error] = "Error: #{@account.errors.full_messages.join("/n")}"
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
   def edit
   end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:name, :balance)
+  end
 end
+
+flash = { success: 'It worked', error: 'It did not work'}
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,5 @@
 class Account < ApplicationRecord
   belongs_to :user
+
+  validates :balance, numericality: {greater_than_or_equal_to: 25, message: 'You are too poor. You need at least a quarter hundo.'}
 end
